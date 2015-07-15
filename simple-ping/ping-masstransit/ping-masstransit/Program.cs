212fd3a573983325ef2d26e32f5d1f4725a98d94
brumburg <brumburg@lopezfoods.com>
2015-07-15 17:23:52 -0500

    Added masstransit ping responder to simple example.

---
+++ simple-ping/ping-masstransit/ping-masstransit/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using MassTransit;
+
+namespace PingMassTransit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceBusFactory.New(sbc =>
+            {
+                sbc.UseRabbitMq();
+                sbc.ReceiveFrom("rabbitmq://localhost/ping-masstransit");
+                sbc.Subscribe(subs =>
+                {
+                    subs.Handler<Ping>((ctx, msg) =>
+                    {
+                        Console.WriteLine("Ping received!");
+                        ctx.Respond(new Pong() { DateTime = DateTime.Now });
+                    });
+                });
+
+                Console.WriteLine("Waiting for Ping...");
+                Console.WriteLine("Press Ctrl+C to exit.");
+            });
+        }
+    }
+}
