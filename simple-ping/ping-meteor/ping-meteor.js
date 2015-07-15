ae387b0aa1462354f41e8c059a62b9f1cd357ec0
Brian Rumburg <brian@rumburg.org>
2015-07-15 17:04:26 -0500

    Updated meteor ping example.

---
+++ simple-ping/ping-meteor/ping-meteor.js
@@ -0,0 +1,56 @@
+if (Meteor.isClient) {
+
+  Template.pong.helpers({
+    lastPong: function(){
+      return Session.get("lastPong");
+    }
+  });
+
+  Template.ping.events({
+    'click .client-ping': function () {
+      console.log('client ping sent');
+      MassTransit.publish('MassTransitEndpoint.Messages:Ping', {});
+    },
+    'click .server-ping': function () {
+      Meteor.call('ping');
+    }
+  });
+
+  MassTransit.inbound.find({}).observe({
+    added: function(doc) {
+      console.log('client pong received')
+      console.log(doc);
+
+      Session.set("lastPong", doc.message.dateTime);
+
+      MassTransit.inbound.remove(doc._id);
+    }
+  });
+
+}
+
+if (Meteor.isServer) {
+
+  MassTransit.init({host: 'localhost', queueName: 'ping-meteor'});
+
+  MassTransit.onConnected = function() {
+    MassTransit.bind('MassTransitEndpoint.Messages:Pong');
+  };
+
+  MassTransit.inbound.find({}).observe({
+    added: function(doc) {
+      console.log('server pong received')
+      console.log(doc);
+
+      MassTransit.inbound.remove(doc._id);
+    }
+  });
+
+  Meteor.methods({
+    ping: function() {
+      console.log('server ping sent');
+      MassTransit.publish('MassTransitEndpoint.Messages:Ping', {});
+    }
+  });
+
+}
